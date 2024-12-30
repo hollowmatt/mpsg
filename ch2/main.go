bbf11db15a78508d763549ffc14ca051a7972f7e
Matt Holloway <hollowmatt@gmail.com>
2024-12-30 16:58:20 -0500

    add a map of languages/phrases

--- ch2/main.go
+++ ch2/main.go
@@ -9,14 +9,18 @@ func main() {
 
 type language string
 
+var phrasebook = map[language]string{
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
 func greet(l language) string {
-	switch l {
-	case "en":
-		return "Hello world"
-	case "fr":
-		return "Bonjour le monde"
-	default:
-		//return a greeting message
-		return ""
+	greeting, ok := phrasebook[l]
+	if !ok {
+		greeting = fmt.Sprintf("unsupported language: %q", l)
 	}
+	return greeting
 }
