13168b47c2d026a57d4f9c25993cb3e53fa8a7e4
Matt Holloway <hollowmatt@gmail.com>
2024-12-30 16:54:43 -0500

    add multi-language support

--- ch2/main_internal_test.go
+++ ch2/main_internal_test.go
@@ -8,12 +8,21 @@ func ExampleMain() {
 	// Hello world
 }
 
-func TestGreet(t *testing.T) {
-	got := greet()
+func TestGreet_en(t *testing.T) {
+	got := greet("en")
 	want := "Hello world"
 
 	if got != want {
 		//mark the test as failed
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestGreet_fr(t *testing.T) {
+	got := greet("fr")
+	want := "Bonjour le monde"
+
+	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
