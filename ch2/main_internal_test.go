04cb4b33e4a45fe14bf53bf754fe8e71581263e4
Matt Holloway <hollowmatt@gmail.com>
2024-12-30 15:58:15 -0500

    refactor to greet function

--- ch2/main_internal_test.go
+++ ch2/main_internal_test.go
@@ -1,7 +1,19 @@
 package main
 
+import "testing"
+
 func ExampleMain() {
 	main()
 	// Output:
 	// Hello world
+}
+
+func TestGreet(t *testing.T) {
+	got := greet()
+	want := "Hello world"
+
+	if got != want {
+		//mark the test as failed
+		t.Errorf("got %q want %q", got, want)
+	}
 }
