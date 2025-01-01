68ece3bc7a8b45fc1340e5a0e219b10ca28c63a2
Matt Holloway <hollowmatt@gmail.com>
2025-01-01 15:50:07 -0500

    setup bookworm to read file and return json content

--- ch3/bookworm.go
+++ ch3/bookworm.go
@@ -1,5 +1,30 @@
 package main
 
-func loadBookworms(filePath string) ([]loadBookworms, error) {
-	return nil, nil
+import (
+	"encoding/json"
+	"os"
+)
+
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:"books"`
+}
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func loadBookworms(filePath string) ([]Bookworm, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var bookworms []Bookworm
+	err = json.NewDecoder(f).Decode(&bookworms)
+	if err != nil {
+		return nil, err
+	}
+	return bookworms, nil
 }
